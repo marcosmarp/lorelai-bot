930e9aa129f3a8a506cf6160152e609536958166
Marcos <marcosmaritnezplermo@gmail.com>
2021-10-09 22:15:40 -0300

    Changed subreddit to production one

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functions import RunBot, InitPraw, sleep
 
 reddit_handler = InitPraw()
-doctor_who_subreddit_handler = reddit_handler.subreddit("lorelaibot")
+doctor_who_subreddit_handler = reddit_handler.subreddit("gilmoregirls")
 
 while(True):
   RunBot(doctor_who_subreddit_handler)
