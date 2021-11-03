e1a1ad9fcfbae919681b15223255741389f09001
Marcos <marcosmartinezpalermo@gmail.com>
2021-11-03 10:01:34 -0300

    Added exception handling

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from functions import RunBot, InitPraw, sleep
+from functions import RunBot, InitPraw, sleep, stderr
+from traceback import print_exc
 
 reddit_handler = InitPraw()
 gilmoregirls_subreddit_handler = reddit_handler.subreddit("gilmoregirls")
 test_subreddit_handler = reddit_handler.subreddit("lorelaibot")
 
 while(True):
-  RunBot(gilmoregirls_subreddit_handler)
+  try:
+    RunBot(gilmoregirls_subreddit_handler)
+  except KeyboardInterrupt: # For quitting with ctrl+C
+    break
+  except:
+    print("Reddit exception: ", file=stderr)
+    print_exc()
+
